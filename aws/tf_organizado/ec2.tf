a2d6109af5dcf97d6ed475a8736ab024cd9a1155
Vanessa Mello <mello.vanessadeoliveira@gmail.com>
2021-12-22 16:03:26 -0300

    azure e gcp

--- aws/tf_organizado/ec2.tf
+++ aws/tf_organizado/ec2.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "dev" {
     # Criar 3 EC2
     count = 3
     # Etapa 1: Selecione uma Imagem de máquina da Amazon (AMI)
-    ami = "ami-04902260ca3d33422"
+    ami = var.amis["us-east-2"]
     # Etapa 2: Escolha um tipo de instância
     instance_type = "t2.micro"
     # Chave SSH, cada região tem a sua, importa no EC2 
@@ -29,7 +29,7 @@ resource "aws_instance" "dev" {
 # Criar um bucket e nova máquina e vincular um ao outro, é multi região
 
 resource "aws_instance" "dev4" {
-    ami = "ami-04902260ca3d33422"
+    ami = var.amis["us-east-2"]
     instance_type = "t2.micro"
     key_name = "terraform-virginia"
     associate_public_ip_address = true
@@ -43,7 +43,7 @@ resource "aws_instance" "dev4" {
 }
 
 resource "aws_instance" "dev5" {
-    ami = "ami-04902260ca3d33422"
+    ami = var.amis["us-east-2"]
     instance_type = "t2.micro"
     key_name = "terraform-virginia"
     associate_public_ip_address = true
@@ -58,7 +58,7 @@ resource "aws_instance" "dev5" {
 # EC2 em outra regiao (Ohio), requer security groups e outra imagem
 resource "aws_instance" "dev6" {
     provider = aws.us-east-2
-    ami = "ami-002068ed284fb165b"
+    ami = var.amis["us-east-2"]
     instance_type = "t2.micro"
     key_name = "terraform-virginia"
     associate_public_ip_address = true
--- aws/tf_organizado/vars.tf
+++ aws/tf_organizado/vars.tf
@@ -1,1 +1,9 @@
 # Arquivo de variáveis
+
+variable "amis" {
+  type = map(string)
+  default = {
+      "us-east-1" = "ami-04902260ca3d33422"
+      "us-east-2" = "ami-002068ed284fb165b"
+  }
+}
