33aff2f4dcc52d9007d073b10efa1379e6f4fd53
Vanessa Mello <35430464+mello-vanessa@users.noreply.github.com>
2022-02-09 16:01:23 -0300

    Update vars.tf

--- aws/tf_organizado/vars.tf
+++ aws/tf_organizado/vars.tf
@@ -8,6 +8,16 @@ variable "amis" {
   }
 }
 
+variable "inst_ami" {
+  type = string
+  default =  "ami-0ed9277fb7eb570c9"
+}
+
+variable "inst_type" {
+  type = string
+  default =  "t2.micro"
+}
+
 variable "chave-ssh" {
    default = "terraform-virginia"
 }
