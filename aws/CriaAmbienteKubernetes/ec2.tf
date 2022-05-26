da91404bd0be710afcad0cc3ec5e1109b9f281fb
Vanessa Mello <vanessa.mello@lojaintegrada.com.br>
2022-05-26 15:28:44 -0300

    up

--- aws/CriaAmbienteKubernetes/ec2.tf
+++ aws/CriaAmbienteKubernetes/ec2.tf
@@ -13,30 +13,6 @@ sudo echo ip_vs_rr >> /etc/modules-load.d/k8s.conf
 sudo echo ip_vs_sh >> /etc/modules-load.d/k8s.conf
 sudo echo ip_vs_wrr >> /etc/modules-load.d/k8s.conf
 sudo echo nf_conntrack_ipv4 >> /etc/modules-load.d/k8s.conf
-sudo apt update
-sudo apt upgrade -y
-curl -fsSL https://get.docker.com | bash
-sudo touch /etc/docker/daemon.json
-sudo cat > /etc/docker/daemon.json <<EOD
-{
-  "exec-opts": ["native.cgroupdriver=systemd"],
-  "log-driver": "json-file",
-  "log-opts": {
-    "max-size": "100m"
-  },
-  "storage-driver": "overlay2"
-}
-EOD
-sudo systemctl daemon-reload
-sudo systemctl restart docker
-sudo apt-get update && sudo apt-get install -y apt-transport-https gnupg2
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-sudo echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update
-sudo apt-get install -y kubelet kubeadm kubectl
-sudo swapoff -a
-sudo kubeadm config images pull
-sudo kubeadm init
 EOF
     
     tags = {
