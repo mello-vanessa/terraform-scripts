bd583a4f74499b96da21eb561e25c218b92a1345
Vanessa Mello <vanessa.mello@lojaintegrada.com.br>
2022-05-27 16:07:38 -0300

    up

--- aws/CriaAmbienteKubernetes/ec2.tf
+++ aws/CriaAmbienteKubernetes/ec2.tf
@@ -16,15 +16,6 @@ echo ip_vs_wrr >> /etc/modules-load.d/k8s.conf
 echo nf_conntrack_ipv4 >> /etc/modules-load.d/k8s.conf
 apt update
 apt upgrade -y
-touch /tmp/daemon.json
-echo "{" > /tmp/daemon.json
-echo "  \"exec-opts\": [\"native.cgroupdriver=systemd\"]," >> /tmp/daemon.json
-echo "  \"log-driver\": \"json-file\","  >> /tmp/daemon.json
-echo "  \"log-opts\": {"  >> /tmp/daemon.json
-echo "    \"max-size\": \"100m\""  >> /tmp/daemon.json
-echo "  },"  >> /tmp/daemon.json
-echo "  \"storage-driver\": \"overlay2\""  >> /tmp/daemon.json
-echo "}"  >> /tmp/daemon.json
 apt-get update && sudo apt-get install -y apt-transport-https gnupg2
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list; exit
