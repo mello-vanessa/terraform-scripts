c4d8afc5c2c79ded9f65ea83bc1d479467a9f87e
Vanessa Mello <vanessa.mello@lojaintegrada.com.br>
2022-05-27 15:35:08 -0300

    up

--- aws/CriaAmbienteKubernetes/ec2.tf
+++ aws/CriaAmbienteKubernetes/ec2.tf
@@ -5,30 +5,46 @@ resource "aws_instance" "worker" {
   key_name                    = var.chave-ssh
   associate_public_ip_address = true
 
-  connection {
-    type        = "ssh"
-    user        = var.user
-    private_key = file(var.key_file)
-    host        = self.public_ip
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo hostnamectl set-hostname k8s-worker${count.index}",
-      "sudo echo 'k8s-worker${count.index}' > /etc/hostname",
-      "echo '127.0.0.1 k8s-worker${count.index}' | sudo tee -a /etc/hosts",
-      "sleep 30",
-      "sudo apt update -y && sudo DEBIAN_FRONTEND=noninteractive apt upgrade -y && sudo apt install ansible -y",
-    ]
-  }
-
-  provisioner "local-exec" {
-    command = "/usr/bin/ansible-playbook -i '${self.public_ip},' -u ${var.user} --private-key=${var.key_file} --ssh-common-args='-o StrictHostKeyChecking=no' ansible/main.yaml"
-  }
+  user_data                   = <<EOF
+#!/bin/bash -xe
+sudo touch /etc/modules-load.d/k8s.conf
+sudo echo br_netfilter >> /etc/modules-load.d/k8s.conf
+sudo echo ip_vs >> /etc/modules-load.d/k8s.conf
+sudo echo ip_vs_rr >> /etc/modules-load.d/k8s.conf
+sudo echo ip_vs_sh >> /etc/modules-load.d/k8s.conf
+sudo echo ip_vs_wrr >> /etc/modules-load.d/k8s.conf
+sudo echo nf_conntrack_ipv4 >> /etc/modules-load.d/k8s.conf
+sudo apt update
+sudo apt upgrade -y
+curl -fsSL https://get.docker.com | bash
+sleep 60
+sudo mkdir /etc/docker
+sudo touch /etc/docker/daemon.json
+sudo echo "{" > /etc/docker/daemon.json
+sudo echo "  \"exec-opts\": [\"native.cgroupdriver=systemd\"]," >> /etc/docker/daemon.json
+sudo echo "  \"log-driver\": \"json-file\","  >> /etc/docker/daemon.json
+sudo echo "  \"log-opts\": {"  >> /etc/docker/daemon.json
+sudo echo "    \"max-size\": \"100m\""  >> /etc/docker/daemon.json
+sudo echo "  },"  >> /etc/docker/daemon.json
+sudo echo "  \"storage-driver\": \"overlay2\""  >> /etc/docker/daemon.json
+sudo echo "}"  >> /etc/docker/daemon.json
+sudo systemctl daemon-reload
+sudo systemctl restart docker
+sudo apt-get update && sudo apt-get install -y apt-transport-https gnupg2
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo su;  echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list; exit
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo swapoff -a
+sudo kubeadm config images pull
+sudo kubeadm init
+EOF
+  
   tags = {
     "Name"      = "k8s-worker${count.index}"
     Environment = terraform.workspace
   }
+
   subnet_id              = aws_subnet.subnet01vpck8s.id
   vpc_security_group_ids = ["${aws_security_group.permite-ssh.id}"]
 }
