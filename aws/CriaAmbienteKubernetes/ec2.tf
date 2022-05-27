3586ce8a8fcecf9fff99930fc68558a4b1926679
Vanessa Mello <vanessa.mello@lojaintegrada.com.br>
2022-05-27 12:19:37 -0300

    up

--- aws/CriaAmbienteKubernetes/ec2.tf
+++ aws/CriaAmbienteKubernetes/ec2.tf
@@ -4,27 +4,28 @@ resource "aws_instance" "worker" {
     instance_type = var.inst_type
     key_name = var.chave-ssh
     associate_public_ip_address = true
-
-  connection {
-    type        = "ssh"
-    user        = var.user
-    private_key = file(var.path-chave-ssh)
-    host        = self.private_ip
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo hostnamectl set-hostname k8s-worker${count.index}",
-      "sudo echo 'k8s-worker${count.index}' > /etc/hostname",
-      "echo '127.0.0.1 k8s-worker${count.index}' | sudo tee -a /etc/hosts",
-      "sleep 30",
-      "sudo apt update -y && sudo DEBIAN_FRONTEND=noninteractive apt upgrade -y && sudo apt install ansible -y",
-    ]
-  }
-
-  provisioner "local-exec" {
-    command = "sleep 10; echo $PATH; /usr/bin/ansible-playbook -i '${self.private_ip},' -u ${var.user} --private-key=${var.path-chave-ssh} --ssh-common-args='-o StrictHostKeyChecking=no' ansible/main.yaml"
-  }
+    user_data                   = <<EOF
+#!/bin/bash -xe
+sudo touch /etc/modules-load.d/k8s.conf
+sudo echo br_netfilter >> /etc/modules-load.d/k8s.conf
+sudo echo ip_vs >> /etc/modules-load.d/k8s.conf
+sudo echo ip_vs_rr >> /etc/modules-load.d/k8s.conf
+sudo echo ip_vs_sh >> /etc/modules-load.d/k8s.conf
+sudo echo ip_vs_wrr >> /etc/modules-load.d/k8s.conf
+sudo echo nf_conntrack_ipv4 >> /etc/modules-load.d/k8s.conf
+sudo apt update
+sudo apt upgrade -y
+curl -fsSL https://get.docker.com | bash
+sudo touch /etc/docker/daemon.json
+sudo echo "{" > /etc/docker/daemon.json
+sudo echo "  \"exec-opts\": [\"native.cgroupdriver=systemd\"]," >> /etc/docker/daemon.json
+sudo echo "  \"log-driver\": \"json-file\","  >> /etc/docker/daemon.json
+sudo echo "  \"log-opts\": {"  >> /etc/docker/daemon.json
+sudo echo "    \"max-size\": \"100m\""  >> /etc/docker/daemon.json
+sudo echo "  },"  >> /etc/docker/daemon.json
+sudo echo "  \"storage-driver\": \"overlay2\""  >> /etc/docker/daemon.json
+sudo echo "}"  >> /etc/docker/daemon.json
+EOF
     
     tags = {
       "Name" = "k8s-worker${count.index}"
